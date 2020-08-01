8814aa81b70b6cd8877de311e57f18bacf03760b
Yury Bialou <Pradd.by@gmail.com>
2020-08-01 10:55:18 +0300

    init

---
+++ app/src/main/java/com/example/rsh_team_7_app/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.rsh_team_7_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
